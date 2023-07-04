1deb83a5856ea029c0e0bdcc169dbf7bf266cf1c
Noura Mousavi <imnouramousavi@gmail.com>
2023-07-04 14:31:13 +0330

    (refactor): fix toggle ball

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(
-  ".container,.movie-list-title,.navbar-container,.toggle,.sidebar,.left-menu-icon"
-);
-
 ball.addEventListener("click", () => {
-  items.forEach((item) => {
-    item.classList.toggle("active");
-  });
   ball.classList.toggle("active");
+  document.body.classList.toggle("dark");
 });
